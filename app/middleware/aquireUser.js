ea85d98d7e769cbc4983ecfab2770df8cb829aa1
Na-aim <fredericksnaaim13@gmail.com>
2022-03-18 15:50:57 +0200

    make it better

--- app/middleware/aquireUser.js
+++ app/middleware/aquireUser.js
@@ -1,11 +1,11 @@
 const User = require("../models/user.model");
 
 getUser = async (req, res, next) => {
-  let product;
+  let user;
   try {
-    product = await User.findById(req.params.id);
-    if (product == null) {
-      return res.status(404).json({ message: "cannot find product" });
+    user = await User.findById(req.params.id);
+    if (user == null) {
+      return res.status(404).json({ message: "cannot find user" });
     }
   } catch (err) {
     return res.status(500).json({ message: err.message });
